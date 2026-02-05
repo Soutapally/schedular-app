f64dd47a510acca2731b8073319df79d935b775f
Soutapally <ruchinetha6@gmail.com>
2026-02-05 15:49:33 +0530

    ER diagram updated ones

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,6 +3,9 @@ import { AppService } from './app.service';
 
 @Controller('hello')
 export class AppController {
+  getHello(): any {
+    throw new Error('Method not implemented.');
+  }
   @Get()
   hello() {
     return {
